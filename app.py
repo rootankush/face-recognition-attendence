2ece2514b0e66e5f784561be1c26f93db1a73104
rootankush <rootankushprompt@gmail.com>
2026-01-20 15:44:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,185 @@
+import cv2
+import os
+import base64
+import io
+from PIL import Image
+from flask import Flask, request, render_template
+from datetime import date
+from datetime import datetime
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import joblib
+
+# Defining Flask App
+app = Flask(__name__)
+
+nimgs = 10
+
+# Saving Date today in 2 different formats
+datetoday = date.today().strftime("%m_%d_%y")
+datetoday2 = date.today().strftime("%d-%B-%Y")
+
+
+# Initializing VideoCapture object to access WebCam
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+# If these directories don't exist, create them
+if not os.path.isdir('Attendance'):
+    os.makedirs('Attendance')
+if not os.path.isdir('static'):
+    os.makedirs('static')
+if not os.path.isdir('static/faces'):
+    os.makedirs('static/faces')
+if f'Attendance-{datetoday}.csv' not in os.listdir('Attendance'):
+    with open(f'Attendance/Attendance-{datetoday}.csv', 'w') as f:
+        f.write('Name,Roll,Time')
+
+
+# get a number of total registered users
+def totalreg():
+    return len(os.listdir('static/faces'))
+
+
+# extract the face from an image
+def extract_faces(img):
+    try:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_points = face_detector.detectMultiScale(gray, 1.2, 5, minSize=(20, 20))
+        return face_points
+    except:
+        return []
+
+
+# Identify face using ML model
+def identify_face(facearray):
+    model = joblib.load('static/face_recognition_model.pkl')
+    return model.predict(facearray)
+
+
+# A function which trains the model on all the faces available in faces folder
+def train_model():
+    faces = []
+    labels = []
+    userlist = os.listdir('static/faces')
+    for user in userlist:
+        for imgname in os.listdir(f'static/faces/{user}'):
+            img = cv2.imread(f'static/faces/{user}/{imgname}')
+            resized_face = cv2.resize(img, (50, 50))
+            faces.append(resized_face.ravel())
+            labels.append(user)
+    faces = np.array(faces)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(faces, labels)
+    joblib.dump(knn, 'static/face_recognition_model.pkl')
+
+
+# Extract info from today's attendance file in attendance folder
+def extract_attendance():
+    df = pd.read_csv(f'Attendance/Attendance-{datetoday}.csv')
+    names = df['Name']
+    rolls = df['Roll']
+    times = df['Time']
+    l = len(df)
+    return names, rolls, times, l
+
+
+# Add Attendance of a specific user
+def add_attendance(name):
+    username = name.split('_')[0]
+    userid = name.split('_')[1]
+    current_time = datetime.now().strftime("%H:%M:%S")
+
+    df = pd.read_csv(f'Attendance/Attendance-{datetoday}.csv')
+    if int(userid) not in list(df['Roll']):
+        with open(f'Attendance/Attendance-{datetoday}.csv', 'a') as f:
+            f.write(f'\n{username},{userid},{current_time}')
+
+
+## A function to get names and rol numbers of all users
+def getallusers():
+    userlist = os.listdir('static/faces')
+    names = []
+    rolls = []
+    l = len(userlist)
+
+    for i in userlist:
+        name, roll = i.split('_')
+        names.append(name)
+        rolls.append(roll)
+
+    return userlist, names, rolls, l
+
+
+## A function to delete a user folder 
+def deletefolder(duser):
+    pics = os.listdir(duser)
+    for i in pics:
+        os.remove(duser+'/'+i)
+    os.rmdir(duser)
+
+
+
+
+################## ROUTING FUNCTIONS #########################
+
+# Our main page
+@app.route('/')
+def home():
+    names, rolls, times, l = extract_attendance()
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2)
+
+
+## List users page
+@app.route('/listusers')
+def listusers():
+    userlist, names, rolls, l = getallusers()
+    return render_template('listusers.html', userlist=userlist, names=names, rolls=rolls, l=l, totalreg=totalreg(), datetoday2=datetoday2)
+
+
+## Delete functionality
+@app.route('/deleteuser', methods=['GET'])
+def deleteuser():
+    duser = request.args.get('user')
+    deletefolder('static/faces/'+duser)
+
+    ## if all the face are deleted, delete the trained file...
+    if os.listdir('static/faces/')==[]:
+        os.remove('static/face_recognition_model.pkl')
+    
+    try:
+        train_model()
+    except:
+        pass
+
+    userlist, names, rolls, l = getallusers()
+    return render_template('listusers.html', userlist=userlist, names=names, rolls=rolls, l=l, totalreg=totalreg(), datetoday2=datetoday2)
+
+
+# Our main Face Recognition functionality. 
+# This function will run when we click on Take Attendance Button.
+@app.route('/process_frame', methods=['POST'])
+def process_frame():
+    data = request.get_json()
+    image_data = data['image'].split(",")[1] # Remove the "data:image/jpeg;base64," header
+    
+    # Convert Base64 string back to an OpenCV image
+    msg = base64.b64decode(image_data)
+    buf = io.BytesIO(msg)
+    img = Image.open(buf)
+    frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+
+    # Use your existing face detection logic
+    faces = extract_faces(frame)
+    if len(faces) > 0:
+        (x, y, w, h) = faces[0]
+        face = cv2.resize(frame[y:y+h, x:x+w], (50, 50))
+        identified_person = identify_face(face.reshape(1, -1))[0]
+        add_attendance(identified_person)
+        return {"status": f"Recognized: {identified_person}"}
+    
+    return {"status": "No face detected"}
+# Our main function which runs the Flask App
+if __name__ == '__main__':
+    app.run(debug=True)
